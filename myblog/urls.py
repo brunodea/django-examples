2c6c70884b36e20dba243e98feba90f57b76c067
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-14 16:34:40 -0200

    view curr_datetime adicionada.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from myblog.views import hello
+from myblog.views import hello, curr_datetime
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/$', include(admin.site.urls)),
-    url(r'^hello/$', hello)
+	url(r'^admin/$', include(admin.site.urls)),
+	url(r'^hello/$', hello),
+	url(r'^time/$', curr_datetime)  
 )
--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from datetime import datetime
 
 def hello(request):
 	return HttpResponse("Hello World")
+
+def curr_datetime(request):
+	now = datetime.now()
+	html = "<html><body>It is %s.</body></html>" % now
+	return HttpResponse(html)
