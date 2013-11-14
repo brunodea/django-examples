ef6a349549677aa522122aea02f46b92da262166
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-14 17:09:14 -0200

    urls dinamicas.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from myblog.views import hello, curr_datetime
+from myblog.views import hello, curr_datetime, hours_ahead
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^admin/$', include(admin.site.urls)),
 	url(r'^hello/$', hello),
-	url(r'^time/$', curr_datetime)  
+	url(r'^time/$', curr_datetime),
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead) #regex que limita em numero de 1 a 2 digitos. \d+ eh para qualquer numero
 )
--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def hello(request):
 	return HttpResponse("Hello World")
 
+#hora depende da time-zone.
 def curr_datetime(request):
 	now = datetime.now()
 	html = "<html><body>It is %s.</body></html>" % now
+	return HttpResponse(html)
+
+def hours_ahead(request, plus):
+	try:
+		plus = int(plus)
+	except ValueError:
+		raise Http404()
+	ahead = datetime.now() + timedelta(hours = plus)
+	html = "<html><body>In %d hour(s), it will be %s.</body></html>" % (plus, ahead)
 	return HttpResponse(html)
