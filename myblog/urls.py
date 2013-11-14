8940c434fdd23381c7433a381a3ec72eb089cf35
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-14 16:03:13 -0200

    regex para página do admin com $ no fim.

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,10 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/$', include(admin.site.urls)),
     url(r'^hello/$', hello)
 )
