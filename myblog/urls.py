0c6a40978c2462e11c7fe998ca10edb9f161f958
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-18 17:08:01 -0200

    personal_datetime adicionado como exemplo de template em codigo.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from myblog.views import hello, curr_datetime, hours_ahead
+from myblog.views import hello, curr_datetime, hours_ahead, personal_datetime
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
 	url(r'^admin/$', include(admin.site.urls)),
 	url(r'^hello/$', hello),
 	url(r'^time/$', curr_datetime),
-	url(r'^time/plus/(\d{1,2})/$', hours_ahead) #regex que limita em numero de 1 a 2 digitos. \d+ eh para qualquer numero
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead), #regex que limita em numero de 1 a 2 digitos. \d+ eh para qualquer numero
+	url(r'^time/personal/$', personal_datetime)
 )
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from datetime import datetime, timedelta
+from django.template import Template, Context
 
 def hello(request):
 	return HttpResponse("Hello World")
@@ -18,3 +19,12 @@ def hours_ahead(request, plus):
 	ahead = datetime.now() + timedelta(hours = plus)
 	html = "<html><body>In %d hour(s), it will be %s.</body></html>" % (plus, ahead)
 	return HttpResponse(html)
+
+def personal_datetime(request):
+	raw_template = "<html><body> Hello, my name is {{ server_name }} and it is {{ time }}.</html></body>"
+
+	t = Template(raw_template)
+	c = Context({"server_name" : "GlaDUS", "time" : datetime.now()})
+
+	return HttpResponse(t.render(c))
+
