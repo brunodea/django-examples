6a75d64f1853d50864fbcbc05d0b963b043e7266
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-19 14:41:53 -0200

    recovering from git fail.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
+from myblog.views import hello, curr_datetime, hours_ahead, conditional_datetime
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/$', include(admin.site.urls)),
+	url(r'^hello/$', hello),
+	url(r'^time/$', curr_datetime),
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead), #regex que limita em numero de 1 a 2 digitos. \d+ eh para qualquer numero
+	#cuidar espaco depois da virgula nos regex.
+	url(r'^time/plus/conditional/(\d{1,2})/$', conditional_datetime)
 )
