72f337fb4cec5d9622f95da7251ff6a83606eb31
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-19 16:05:09 -0200

    ajustei o views.py e urls.py.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, include, url
-from myblog.views import hello, curr_datetime, hours_ahead, conditional_datetime
+from myblog.views import curr_datetime, hours_ahead
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/$', include(admin.site.urls)),
-	url(r'^hello/$', hello),
+	url(r'^$', curr_datetime),
 	url(r'^time/$', curr_datetime),
-	url(r'^time/plus/(\d{1,2})/$', hours_ahead), #regex que limita em numero de 1 a 2 digitos. \d+ eh para qualquer numero
-	#cuidar espaco depois da virgula nos regex.
-	url(r'^time/plus/conditional/(\d{1,2})/$', conditional_datetime)
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
--- myblog/views.py
+++ myblog/views.py
@@ -1,17 +1,10 @@
-from django.http import HttpResponse
 from datetime import datetime, timedelta
-from django.template import Template, Context
-from django.template.loader import get_template
 from django.shortcuts import render
-
-def hello(request):
-	return HttpResponse("Hello World")
 
 #hora depende da time-zone.
 def curr_datetime(request):
 	now = datetime.now()
-	html = "<html><body>It is %s.</body></html>" % now
-	return HttpResponse(html)
+	return render(request, 'home.html', {'time':now})
 
 def hours_ahead(request, plus):
 	try:
@@ -19,24 +12,7 @@ def hours_ahead(request, plus):
 	except ValueError:
 		raise Http404()
 	ahead = datetime.now() + timedelta(hours = plus)
-	html = "<html><body>In %d hour(s), it will be %s.</body></html>" % (plus, ahead)
-	return HttpResponse(html)
-
-def conditional_datetime(request, plus):
-	
-	invalid = False
-	try:
-		plus = int(plus)
-	except ValueError:
-		invalid = True
-		plus = 0
-
-	invalid = plus > 24
-
-	c = {"server_name" : "GlaDUS", "time" : datetime.now() + timedelta(hours = plus), "invalid_plus": invalid,
-		"nav_template": "nav.html", "current_section": "Conditional Datetime"}
-
-	return render(request, 'home.html', c)
+	return render(request, 'future.html', {'hour_offset':plus, 'next_time':ahead})
 
 
 
