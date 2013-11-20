756a007f59722bd48378cf72408c044de62ec736
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-20 18:41:22 -0200

    adicionei template e stuff para formulario de busca.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from myblog.views import curr_datetime, hours_ahead
+from books.views import search_form, search
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', curr_datetime),
+	url(r'^search-form/$', search_form),
+	url(r'^search/$', search),
 	url(r'^time/$', curr_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+	url(r'^$', curr_datetime),
 )
