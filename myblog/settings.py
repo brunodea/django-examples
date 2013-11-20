d18eaab27d6f8e1ab1d87c92197a938b36507a75
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-20 15:32:04 -0200

    adicionei o DB nos settings.

--- myblog/settings.py
+++ myblog/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+	'USER': 'bruno',
+	'PASSWORD': 'd6qjf3',
+	'HOST': 'localhost', #localhost
+	'PORT': '5432', #default
     }
 }
 
