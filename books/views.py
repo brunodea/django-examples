ef3f47a3ff9ebdeeabb162bd401148467bc57b3d
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-21 15:23:37 -0200

    form vazio apresentando erro na mesma página.

--- books/views.py
+++ books/views.py
@@ -7,9 +7,11 @@ def search_form(request):
 	return render(request, 'search_form.html')
 
 def search(request):
+	context = {'error': True}
+	page = 'search_form.html'
 	if 'q' in request.GET and request.GET['q']:
 		q = request.GET['q']
 		books = Book.objects.filter(title__icontains=q) #case insensitive
-		return render(request, 'search_results.html', {'books': books, 'query': q})
-	else:
-		return HttpResponse('Please submit a search term.')
+		context = {'books': books, 'query': q}
+		page = 'search_results.html'
+	return render(request, page, context)
