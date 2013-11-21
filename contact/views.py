aae87315e199a312a89c3f4cea35a9b20751e2b0
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-11-21 16:14:16 -0200

    adicionei nas views e urlconf, a página de agradecimento pelo contato.

--- contact/views.py
+++ contact/views.py
@@ -7,14 +7,17 @@ def contact(request):
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
 		if form.is_valid():
-			cd = form.cleaned_data
-			send_mail(
-				cd['subject'],
-				cd['message'],
-				cd.get('email', 'noreply@example.com'),
-				['brunodea@inf.ufsm.br'],
-			)
+			#cd = form.cleaned_data
+			#send_mail(
+			#	cd['subject'],
+			#	cd['message'],
+			#	cd.get('email', 'noreply@example.com'),
+			#	['brunodea@inf.ufsm.br'],
+			#)
 			return HttpResponseRedirect('/contact/thanks/')
 	else:
-		form = ContactForm()
+		form = ContactForm() #primeira vez acessando a pagina.
 	return render(request, 'contact_form.html', {'form': form})
+
+def contact_thanks(request):
+	return render(request, 'contact_thanks.html', {})
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from myblog.views import curr_datetime, hours_ahead
 from books.views import search_form, search
+from contact.views import contact, contact_thanks
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,5 +11,7 @@ urlpatterns = patterns('',
 	url(r'^search/$', search),
 	url(r'^time/$', curr_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+	url(r'^contact/$', contact),
+	url(r'^contact/thanks/$', contact_thanks),
 	url(r'^$', curr_datetime),
 )
